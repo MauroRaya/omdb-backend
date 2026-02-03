96abec844ad926cd261a7dc16ecd55222c150895
MauroRaya <maurorayabr@gmail.com>
2026-02-03 17:09:14 -0300

    feat: create swagger config

---
+++ src/config/swagger.config.ts
@@ -0,0 +1,14 @@
+import { INestApplication } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+export function configureSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('API OMDb')
+    .setDescription('API de busca de filmes e séries usando a API do OMDb')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
+}
