392f91d03bbfa61082bd273e08e272916fbcc20c
MauroRaya <maurorayabr@gmail.com>
2026-02-03 22:52:40 -0300

    feat: import cache module in omdb module

--- src/omdb/omdb.module.ts
+++ src/omdb/omdb.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { OmdbService } from './omdb.service';
 import { OmdbController } from './omdb.controller';
 import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    HttpModule,
+    CacheModule.register(),
+  ],
   providers: [OmdbService],
   controllers: [OmdbController],
 })
