6ce76d09280f3bc8238920880ec2e1969e3164c2
MauroRaya <maurorayabr@gmail.com>
2026-02-03 22:52:57 -0300

    feat: implement cache logic in fetch by title method

--- src/omdb/omdb.service.ts
+++ src/omdb/omdb.service.ts
@@ -1,23 +1,31 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { EnvService } from 'src/env/env.service';
 import { HttpService } from '@nestjs/axios';
 import { lastValueFrom } from 'rxjs';
+import { Cache, CACHE_MANAGER } from '@nestjs/cache-manager';
 
 @Injectable()
 export class OmdbService {
   constructor(
     private readonly envService: EnvService,
     private readonly httpService: HttpService,
+    @Inject(CACHE_MANAGER) private readonly cacheManager: Cache
   ) {}
 
   async fetchByTitle(title: string) {
+    const value = await this.cacheManager.get(title);
+    if (value) {
+      return value;
+    }
+
     const apiBaseUrl = this.envService.get<string>('OMDB_API_BASE_URL');
     const apiKey = this.envService.get<string>('OMDB_API_KEY');
 
-    const response = await lastValueFrom(
+    const { data } = await lastValueFrom(
       this.httpService.get(`${apiBaseUrl}/?apikey=${apiKey}&t=${title}`),
-    );
+    ) ?? {};
 
-    return response.data;
+    await this.cacheManager.set(title, data, 1000 * 60 * 5);
+    return data;
   }
 }
