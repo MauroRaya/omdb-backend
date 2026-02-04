c9a79c9bb66d52450fcb5ce48e044e8afccfa6fd
MauroRaya <maurorayabr@gmail.com>
2026-02-03 23:09:55 -0300

    refactor: use fetch by title dto in controller

--- src/omdb/omdb.controller.ts
+++ src/omdb/omdb.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get, Query } from '@nestjs/common';
 import { OmdbService } from './omdb.service';
+import { FetchByTitleDTO } from './dto/fetch-by-title.dto';
 
 @Controller('omdb')
 export class OmdbController {
   constructor(private readonly omdbService: OmdbService) {}
 
   @Get()
-  async getByTitle(@Query('title') title: string): Promise<any> {
+  async getByTitle(@Query('title') title: string): Promise<FetchByTitleDTO> {
     return await this.omdbService.fetchByTitle(title);
   }
 }
